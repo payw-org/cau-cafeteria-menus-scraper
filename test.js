27e4d442f02c860436f5464a9daeaacf5c0fd69c
jhaemin <io@jhaemin.com>
2020-09-23 22:54:52 +0900

    test: test with the original source code

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 const account = require('./account')
-const { CCMS } = require('./dist/index')
+const { CCMS } = require('./src/index')
 
 CCMS({
   id: account.id,
   pw: account.pw,
-  days: 2
+  days: 3
 })
   .then(data => {
     console.log(JSON.stringify(data, null, 2))
